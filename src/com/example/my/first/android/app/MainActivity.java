fea2939ce70b672d9f34c08d7758e338e0b7b634
Valérian Pittet <valerian.pittet@epfl.ch>
2012-09-23 15:11:14 +0200

    HW0 commit

---
+++ src/com/example/my/first/android/app/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.my.first.android.app;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }
+    
+    public void displayMessage(View view){
+    	Toast.makeText(this, "You clicked Display!", Toast.LENGTH_SHORT).show();
+    	Intent	displayActivityIntent = new Intent(this, ShowMessageActivity.class);
+    	EditText editText = (EditText) findViewById(R.id.edit_text);
+    	String userText = editText.getText().toString();
+    	displayActivityIntent.putExtra(this.getClass().getName(), userText);
+    	startActivity(displayActivityIntent);
+    }
+    
+    
+}
---
+++ src/com/example/my/first/android/app/ShowMessageActivity.java
@@ -0,0 +1,26 @@
+package com.example.my.first.android.app;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.widget.TextView;
+
+public class ShowMessageActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_message);
+        Intent startingIntent = getIntent();
+        String userText = startingIntent.getStringExtra(MainActivity.class.getName());
+        TextView textView = (TextView) findViewById(R.id.displayed_text);
+        textView.setText(userText);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_show_message, menu);
+        return true;
+    }
+}
